e37ea8097f8d0d6ff1c08daa85863b197a31d359
j-doka <jdoka42@gmail.com>
2024-03-10 17:21:16 +0000

    turned into a class

--- main.py
+++ main.py
@@ -1,78 +1,73 @@
-# building a lexicon of positive and negative words
-
 import spacy
 from spacy_wordnet.wordnet_annotator import WordnetAnnotator
 import json
 import pandas as pd
 from typing import List, Tuple
 
-# Load the spaCy model
-nlp = spacy.load("en_core_web_sm")
-nlp.add_pipe("spacy_wordnet", after='tagger')
+class LexiconBuilder:
+    def __init__(self, review):
+        self.nlp = spacy.load("en_core_web_sm")
+        self.nlp.add_pipe("spacy_wordnet", after='tagger')
 
-# seed attributes
-# https://www.cs.uic.edu/~liub/FBS/sentiment-analysis.html
+        self.personality_traits = ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Neuroticism"]
+        self.emotional_states = ["anger", "fear", "joy", "sadness", "surprise"]
+        self.interpersonal_skills = ["empathy", "assertiveness", "conflict", "leadership", "teamwork"]
 
-personality_traits = ["Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Neuroticism"]
-emotional_states = ["anger", "fear", "joy", "sadness", "surprise"]
-interpersonal_skills = ["empathy", "assertiveness", "conflict", "leadership", "teamwork"]
+        self.seed_lexicon = {
+            "PersonalityTraits": self.personality_traits,
+            "EmotionalStates": self.emotional_states,
+            "InterpersonalSkills": self.interpersonal_skills
+        }
 
-seed_lexicon = {
-    "PersonalityTraits": personality_traits,
-    "EmotionalStates": emotional_states,
-    "InterpersonalSkills": interpersonal_skills
-}
+        self.review = review 
+        self.seed_hla_lexicon = {}
 
-review = 'data/reviews.pkl' 
+    def init_lexicon(self):
+        for key in self.seed_lexicon:
+            self.seed_hla_lexicon[key] = {}
+            for word in self.seed_lexicon[key]:
+                self.seed_hla_lexicon[key][word] = {"synonyms": [], "antonyms": []}
 
-# use wordnet to create seed lexicon
+    def get_synonyms_antonyms(self, word) -> Tuple[List[str], List[str]]:
+        synonyms = []
+        antonyms = []
+        for syn in word._.wordnet.synsets():
+            for lemma in syn.lemmas():
+                synonyms.append(lemma.name())
+                if lemma.antonyms():
+                    antonyms.append(lemma.antonyms()[0].name())
+        return synonyms, antonyms
 
-seed_emotions_lexicon = {}
+    def get_definitions(self, word):
+        definitions = []
+        for syn in word._.wordnet.synsets():
+            definitions.append(syn.definition())
+        return definitions
 
-def init_lexicon():
-    for key in seed_lexicon:
-        seed_emotions_lexicon[key] = {}
-        for word in seed_lexicon[key]:
-            seed_emotions_lexicon[key][word] = {"synonyms": [], "antonyms": []}
+    def polarity_score(self, word):
+        # get the polarity score of the word
+        pass
 
-def get_synonyms_antonyms(word) -> Tuple[List[str], List[str]]:
-    synonyms = []
-    antonyms = []
-    for syn in word._.wordnet.synsets():
-        for lemma in syn.lemmas():
-            synonyms.append(lemma.name())
-            if lemma.antonyms():
-                antonyms.append(lemma.antonyms()[0].name())
-    return synonyms, antonyms
+    def create_lexicon(self):
+        for key in self.seed_lexicon:
+            for word in self.seed_lexicon[key]:
+                synonyms, antonyms = self.get_synonyms_antonyms(self.nlp(word)[0])
+                self.seed_hla_lexicon[key][word]["synonyms"] = synonyms
+                self.seed_hla_lexicon[key][word]["antonyms"] = antonyms
+                self.seed_hla_lexicon[key][word]["definitions"] = self.get_definitions(self.nlp(word)[0])
 
-def get_definitions(word):
-    definitions = []
-    for syn in word._.wordnet.synsets():
-        definitions.append(syn.definition())
-    return definitions
+    def save_lexicon(self):
+        with open('data/seed_hla_lexicon.json', 'w') as f:
+            json.dump(self.seed_hla_lexicon, f)
 
-def polarity_score(word):
-    # get the polarity score of the word
-    pass
-
-
-
-def create_lexicon():
-    for key in seed_lexicon:
-        for word in seed_lexicon[key]:
-            synonyms, antonyms = get_synonyms_antonyms(nlp(word)[0])
-            seed_emotions_lexicon[key][word]["synonyms"] = synonyms
-            seed_emotions_lexicon[key][word]["antonyms"] = antonyms
-            seed_emotions_lexicon[key][word]["definitions"] = get_definitions(nlp(word)[0])
-
-def main():
-    init_lexicon()
-    create_lexicon()
-    with open('data/seed_emotions_lexicon.json', 'w') as f:
-        json.dump(seed_emotions_lexicon, f)
-    print(seed_emotions_lexicon)
+    def print_lexicon(self):
+        print(self.seed_hla_lexicon)
 
 if __name__ == "__main__":
-    main()
-    
-                                                                                                               
+    lexicon_builder = LexiconBuilder('data/reviews.pkl')
+    lexicon_builder.init_lexicon()
+    lexicon_builder.create_lexicon()
+    lexicon_builder.save_lexicon()
+    lexicon_builder.print_lexicon()
+
+    # https://ryanong.co.uk/2020/08/01/day-214-learn-nlp-with-me-slp-textbook-lexicons-for-sentiment-affect-and-connotation-ii/
