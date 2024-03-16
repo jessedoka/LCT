8e4858d95b9d3290f7d90b00ee8ec7fab22993df
j-doka <jdoka42@gmail.com>
2024-03-16 20:51:19 +0000

    corrections for seed expansion using wordnet

--- lexicon_construction.py
+++ lexicon_construction.py
@@ -2,6 +2,9 @@ import numpy as np
 from gensim.models import Word2Vec
 import networkx as nx
 import pandas as pd
+
+import matplotlib.pyplot as plt
+
 import nltk
 from nltk.corpus import opinion_lexicon
 from nltk.corpus import stopwords
@@ -10,9 +13,6 @@ from nltk import pos_tag
 
 from nltk.corpus import wordnet
 import string
-
-
-
 
 def initialize_lexicon():
     C, Ld, Ln = set(), set(), set()
@@ -34,8 +34,6 @@ def extract_sentiment_terms(sentence):
 
     return sentiment_terms
 
-# wordnet expansion 
-
 def get_synonyms(word):
     synonyms = set()
     for syn in wordnet.synsets(word):
@@ -50,11 +48,11 @@ def preprocess_corpus(corpus):
 
     # read the csv file only return the review_text
     df = pd.read_csv(corpus)
-    processed_corpus = df['review_text'].tolist()
+    reviews =  df['review_text'].tolist()
     
     # get sentences from each review
     sentences = []
-    for review in processed_corpus:
+    for review in reviews:
         sentences.extend(nltk.sent_tokenize(review))
 
     # Extract sentiment terms from each sentence
@@ -63,10 +61,7 @@ def preprocess_corpus(corpus):
         sentiment_terms.update(extract_sentiment_terms(sentence))
 
     # Add the sentiment terms to the processed corpus
-    processed_corpus.append(list(sentiment_terms))
-
-    with open("output/sentiment_terms.txt", "w") as f:
-        f.write(' '.join(sentiment_terms))
+    processed_corpus = [word_tokenize(term) for term in sentiment_terms]
 
     return processed_corpus
 
@@ -75,6 +70,8 @@ def learn_word_embeddings(processed_corpus):
     # Train a Word2Vec model on the processed corpus
     model = Word2Vec(sentences=processed_corpus,
                      vector_size=100, window=5, min_count=1, workers=4)
+    
+    print(model.wv.similarity('cry', 'good'))
     return model
 
 
@@ -82,15 +79,28 @@ def expand_seeds(seeds, model, Tc):
     # TODO: Implement the expansion of seeds using wordnet 
 
     # if model cannot find word in seed then use wordnet to find synonyms 
-    
 
+    # Pre-compute a dictionary of similarities 
+    similarities = {seed: {} for seed in seeds}
+    for word in model.wv.index_to_key:
+        for seed in seeds:
+            if seed in model.wv.index_to_key:
+                similarities[seed][word] = model.wv.similarity(seed, word)
+            else:
+                synonyms = get_synonyms(seed)
+                for synonym in synonyms:
+                    if synonym in model.wv.index_to_key:
+                        similarities[synonym][word] = model.wv.similarity(synonym, word)
+                else:
+                    similarities[seed][word] = 0
+    print(similarities)
     C = set()
-    for Si in seeds:
-        for Wj in model.wv.index_to_key:
-            if model.wv.similarity(Si, Wj) >= Tc:
-                # Adding both the seed and the similar word to the candidate set
-                C.add((Si, Wj))
-    return C
+    for seed, similar_words in similarities.items():
+        for word, similarity in similar_words.items():
+            if similarity >= Tc:
+                C.add((seed, word))     
+                     
+    return C, seeds
 
 
 def build_semantic_graph(C, model):
@@ -100,8 +110,9 @@ def build_semantic_graph(C, model):
         Si, Wj = word_pair
         # Add an edge between Si and Wj with weight as their similarity
         G.add_edge(Si, Wj, weight=model.wv.similarity(Si, Wj))
-    return G
 
+    print(f"graph edges: {G.edges()} -> {len(G.edges())} edges")
+    return G
 
 def label_propagation(G, seeds, max_iterations=100):
     # Initialize labels based on seeds
@@ -114,12 +125,15 @@ def label_propagation(G, seeds, max_iterations=100):
         for node in G.nodes():
             if node not in seeds:  # Don't update seed labels
                 # Update label based on the weighted average of neighbor labels
+                # print neighbors
                 labels[node] = int(np.mean([labels[neighbor]
                                    for neighbor in G.neighbors(node)]))
 
         # Check for convergence
         if prev_labels == labels:
             break
+
+    print(f"labels: {labels}")
     return labels
 
 
@@ -137,35 +151,37 @@ def main(corpus, seeds, Tc):
     C, Ld, Ln = initialize_lexicon()
     processed_corpus = preprocess_corpus(corpus)
     model = learn_word_embeddings(processed_corpus)
-    C = expand_seeds(seeds, model, Tc)
+    # C, seeds = expand_seeds(seeds, model, Tc)
 
-    G = build_semantic_graph(C, model)
+    # G = build_semantic_graph(C, model)
 
-    labels = label_propagation(G, seeds)
-    Ld, Ln = build_lexicon(labels)
-    L = Ld.union(Ln)
-    return L, Ld, Ln, G, C
+    # labels = label_propagation(G, seeds)
+    # Ld, Ln = build_lexicon(labels)
+    # L = Ld.union(Ln)
+    # return L, Ld, Ln, G, C
+    return None, None, None, None, None
 
 
 # Example usage
-corpus = 'data/reviews.csv' 
+corpus = 'data/tinysample.csv' 
 # big five personality traits
 seeds = {"anger": -1, "fear": -1, "joy": 1, "sadness": -1, "surprise": 1}
 
 Tc = 0.5  # Threshold for similarity
 L, Ld, Ln, G, C = main(corpus, seeds, Tc)
 
-with open('output/lexicon.txt', 'w') as f:
-    f.write(' '.join(L))
+# with open('output/lexicon.txt', 'w') as f:
+#     f.write(' '.join(L))
 
-with open('output/depressive_lexicon.txt', 'w') as f:
-    f.write(' '.join(Ld))
+# with open('output/depressive_lexicon.txt', 'w') as f:
+#     f.write(' '.join(Ld))
 
-with open('output/non_depressive_lexicon.txt', 'w') as f:
-    f.write(' '.join(Ln))
+# with open('output/non_depressive_lexicon.txt', 'w') as f:
+#     f.write(' '.join(Ln))
 
-with open('output/graph.txt', 'w') as f: 
-    f.write(str(G.edges()))
+# with open('output/graph.txt', 'w') as f: 
+#     f.write(str(G.edges()))
 
-with open('output/candidate.txt', 'w') as f: 
-    f.write(str(C))
+# with open('output/candidate.txt', 'w') as f: 
+#     f.write(str(C))
+
