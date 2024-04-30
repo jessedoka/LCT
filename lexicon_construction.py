75d576826f9345c739f2eb84243936667924951d
jessedoka <jdoka42@gmail.com>
2024-04-30 14:02:37 +0100

    Update lexicon_construction.py with new CorpusStreamer class and modify learn_word_embeddings function

--- lexicon_construction.py
+++ lexicon_construction.py
@@ -10,10 +10,11 @@ from collections import defaultdict
 import nltk
 from nltk.corpus import sentiwordnet as swn
 
+import gzip
 from tqdm import tqdm
 import numpy as np
 
-from preprocessing import preprocess_corpus 
+from preprocessing import preprocess_text, preprocess_corpus
 from sklearn.neighbors import NearestNeighbors
 
 nltk.download('opinion_lexicon')
@@ -30,11 +31,25 @@ def get_synonyms(word):
             synonyms.add(lemma.name())
     return synonyms
 
-def learn_word_embeddings(processed_corpus):
+# This will crash computer!
+class CorpusStreamer(object):
+    def __init__(self, gzip_file_path, preprocess_text=preprocess_text):
+        self.gzip_file_path = gzip_file_path
+        self.preprocess_text = preprocess_text
+
+    def __iter__(self):
+        with gzip.open(self.gzip_file_path, 'rt') as file:
+            print("Training...")
+            for line in tqdm(file):
+                processed_text, sentiment_terms = self.preprocess_text(line)
+                yield processed_text, sentiment_terms
+
+def learn_word_embeddings(gzip_file_path):
     print("learning word embeddings")
+    processed_corpus, sentiment_terms = CorpusStreamer(gzip_file_path)
     model = Word2Vec(sentences=processed_corpus,
                      vector_size=100, window=5, min_count=1, workers=4)
-    return model.wv
+    return model.wv, sentiment_terms
 
 
 def expand_seeds(seeds, model, Tc, sentiment_terms):
@@ -129,9 +144,9 @@ def build_lexicon(labels):
     return {key: list(value) for key, value in lexicon.items()}
 
 def construct(corpus, seeds, Tc, text):
-    processed_corpus, sentiment_terms = preprocess_corpus(corpus, text)
-    # model = learn_word_embeddings(processed_corpus)
-    model = api.load("word2vec-google-news-300")
+    # _, sentiment_terms = preprocess_corpus(corpus, text)
+    model, sentiment_terms = learn_word_embeddings(corpus) 
+    # model = api.load("word2vec-google-news-300")
     C = expand_seeds(seeds, model, Tc, sentiment_terms)
 
     G = build_semantic_graph(C, model)
